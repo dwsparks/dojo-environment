63f48224e63d2e6a27b36864b27292810d556106
dwsparks <dustins@SparksAir.local>
2014-03-02 14:25:57 -0400

    updating bootstrap to get dojo correctly

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -10,14 +10,14 @@ apt-get install -y libapache2-mod-php5
 /etc/init.d/apache2 restart
 
 #get dojo source and demos
-wget -O ~/ http://download.dojotoolkit.org/release-1.8.6/dojo-release-1.8.6-demos.tar.gz
-wget -O ~/ http://download.dojotoolkit.org/release-1.8.6/dojo-release-1.8.6-src.tar.gz
+wget -O /tmp/dojo-release-1.8.6-demos.tar.gz http://download.dojotoolkit.org/release-1.8.6/dojo-release-1.8.6-demos.tar.gz
+wget -O /tmp/dojo-release-1.8.6-src.tar.gz http://download.dojotoolkit.org/release-1.8.6/dojo-release-1.8.6-src.tar.gz
 
-tar -xzf ~/ dojo-release-1.8.6-demos.tar.gz
-tar -xzf ~/ dojo-release-1.8.6-src.tar.gz
+tar -xzf /tmp/dojo-release-1.8.6-demos.tar.gz
+tar -xzf /tmp/dojo-release-1.8.6-src.tar.gz
 
-mv ~/ dojo-release-1.8.6/demos /var/www/
-mv ~/ dojo-release-1.8.6-src/* /var/www/
+mv /tmp/dojo-release-1.8.6/demos /var/www/
+mv /tmp/dojo-release-1.8.6-src/* /var/www/
 
 #install Git
 apt-get install -y git-core
